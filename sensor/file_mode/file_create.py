88cd8d66c67a797321e0e8a860c424a03ab54fe3
jxy <xyyy1420@gmail.com>
2022-08-05 20:21:26 +0800

    fix bug

--- sensor/file_mode/file_create.py
+++ sensor/file_mode/file_create.py
@@ -1,4 +1,5 @@
 from cmath import log
+import logging
 import os
 
 # TODO：需要测试文件夹们是否存在，如果存在就不需要创建
@@ -6,14 +7,28 @@ import os
 
 def create_new_path(base_path, sensor_id):
     finall_path = os.path.join(base_path, sensor_id)
-    log_path = os.path.join(finall_path, 'log', '/')
-    rule_path = os.path.join(finall_path, 'rules', '/')
-    csv_path = os.path.join(finall_path, 'csvs', '/')
+    log_path = os.path.join(finall_path, 'log/')
+    rule_path = os.path.join(finall_path, 'rules/')
+    csv_path = os.path.join(finall_path, 'csvs/')
 
-    os.makedirs(finall_path)
-    os.makedirs(log_path)
-    os.makedirs(rule_path)
-    os.makedirs(csv_path)
+    if not os.path.exists(finall_path):
+        os.makedirs(finall_path)
+    else:
+        file_exist(finall_path)
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    else:
+        file_exist(finall_path)
+
+    if not os.path.exists(rule_path):
+        os.makedirs(rule_path)
+    else:
+        file_exist(finall_path)
+
+    if not os.path.exists(csv_path):
+        os.makedirs(csv_path)
+    else:
+        file_exist(finall_path)
 
     return {
         "sensor_path": finall_path,
@@ -21,3 +36,7 @@ def create_new_path(base_path, sensor_id):
         "rule_path": rule_path,
         "csv_path": csv_path
     }
+
+
+def file_exist(path):
+    logging.warn(f"file exist {path}")
