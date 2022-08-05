057b70625b5de97abfa432a158d8ee66b19636f8
jxy <xyyy1420@gmail.com>
2022-08-05 22:19:59 +0800

    fix bug

--- sensor/sensor_role.py
+++ sensor/sensor_role.py
@@ -59,11 +59,11 @@ class Sensor(object):
         # self.file_monitor = 'file_monitor'
 
     def start(self):
+        self.load_sensor()
         if self.data['mode'] != 'deep_learn_ids':
             target = Process(target=self.load_log_deal)
             target.start()
             target.join()
-        self.load_sensor()
         # DONE：需要判断是否成功
 
     def load_deep_learn(self):
