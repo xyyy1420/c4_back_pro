6f7413141267c57ac8448e571f8e6d70a5525624
jxy <xyyy1420@gmail.com>
2022-08-05 11:51:02 +0800

    fix path bug

--- sensor/sensor_sole.py
+++ sensor/sensor_sole.py
@@ -4,7 +4,7 @@ import sys
 import os
 from multiprocessing import Process
 
-from .predict.file_monitor import MyEventHandler
+from .predict.file_monitor.file_monitor import FileEventHandler
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
