633cff04fccbe105f85f24adadf8070c6223e84b
jxy <xyyy1420@gmail.com>
2022-08-15 19:21:33 +0800

    fix bug

--- sensor/log_deal/log_mode/snort_log.py
+++ sensor/log_deal/log_mode/snort_log.py
@@ -2,13 +2,13 @@ import os
 import socket
 import logging
 import sqlite3
-# from ..snortunsock import alert
+from ..snortunsock import alert
 import dpkt
 import time
 from ..log_sender import log_sender
 from .iptable import insert_rule, del_rule
 from ...ip_info import get_country
-from snortunsock import alert
+# from snortunsock import alert
 
 
 class LogReceive(object):
