daea68690c08352eca2741a1128179078e5682ce
jxy <xyyy1420@gmail.com>
2022-08-21 19:55:59 +0800

    add pcap path

--- sensor/predict/file_monitor/file_monitor.py
+++ sensor/predict/file_monitor/file_monitor.py
@@ -50,6 +50,7 @@ class FileEventHandler(FileSystemEventHandler):
 
 def cicflow(input_path, output_path, id):
     logging.warn(input_path, output_path)
+    subprocess.Popen(f"touch {output_path}")
     with suppress_stdout_stderr():
         res = subprocess.Popen(
             f"cicflowmeter -f {input_path} -c {output_path}", shell=True)
