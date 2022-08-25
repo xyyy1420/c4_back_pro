881d230f34643823fdd88b144f8314d0330f483c
jxy <xyyy1420@gmail.com>
2022-08-25 19:34:35 +0800

    add sbi doc

--- mode_test.py
+++ mode_test.py
@@ -17,7 +17,7 @@ from sensor.sensor_role import Sensor
 
 if __name__ == '__main__':
     data = {
-        'id': '10000002',
+        'id': '10000003',
         'rule_file': '/usr/local/snort/rules/local.rules',
         # 'mode': 'interface_ids',
         'mode': 'deep_learn_ids',
